ccca94fd8384e192ea8f7520f835a7e013debfc5
system1970 <64970890+system1970@users.noreply.github.com>
2020-05-30 23:03:34 +0530

    Add files via upload

---
+++ CodeForces.py
@@ -0,0 +1,34 @@
+import requests
+import datetime
+import os
+from bs4 import BeautifulSoup
+#Problem
+r = requests.get("https://codeforces.com/api/user.status?handle=pracurser&from=1&count=50")
+json_format_before = r.json()
+json_format = []
+for i in range(50):
+    timestamp = json_format_before["result"][i]["creationTimeSeconds"]
+    value = datetime.datetime.fromtimestamp(timestamp)
+    if (datetime.datetime.now().day==value.day):
+        json_format.append(json_format_before["result"][i])
+    else:
+        break
+print()
+problems = []
+if len(json_format)!=0:
+    for i in range(len(json_format)):
+        if json_format[i]["verdict"]=="OK" and json_format[i]["problem"]["name"] not in problems:
+            Id = json_format[i]["id"]
+            ContestId = json_format[i]["contestId"]
+            problems.append(json_format[i]["problem"]["name"])
+            # Solution to Problem:
+            Submission = requests.get("https://codeforces.com/contest/"+str(ContestId)+"/submission/"+str(Id))
+            soup = BeautifulSoup(Submission.content,features="html.parser")
+            mydivs = soup.findAll("pre", attrs={"id": "program-source-text"})
+            Solution = str(mydivs).replace('[<pre class="prettyprint lang-py linenums program-source" id="program-source-text" style="padding: 0.5em;">','https://codeforces.com/problemset/problem/'+str(ContestId)+'/'+str(json_format[i]["problem"]["index"])+"\n\n"+"              "+str(json_format[i]["problem"]["name"])+":"+"\n\n").replace("</pre>]","")
+            script_dir = os.path.dirname(__file__)
+            file_path = os.path.join(script_dir, "./Codeforces/"+str(json_format[i]["problem"]["index"])+"/"+str(json_format[i]["problem"]["name"])+".txt")
+            file = open(file_path, "w") 
+            file.write(Solution) 
+            file.close() 
+            print()
